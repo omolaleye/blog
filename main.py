5731d5d3c1f500af93c0129b284133e3bb23cf0c
O.M Olaleye <info@omlyms.com>
2022-02-03 09:45:54 +0100

    General house keeping with os.environ

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ from os import environ
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config['SECRET_KEY'] = os.environ["APP_CONFIG_SECRET_KEY"]
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
-MY_EMAIL = "info@omlyms.com"
-MY_PASSWORD = "ncsgixzmgywnqxpa"
+MY_EMAIL =  os.environ["EMAIL"]
+MY_PASSWORD = os.environ["EMAIL_PASS"]
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
